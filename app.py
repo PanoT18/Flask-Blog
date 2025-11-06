d10887690f8ef689dcc2dc7a3279d8bd49ccf51c
Pano Tsiftilis <panot@macbookpro.lan>
2025-11-06 15:06:25 -0600

    Added functionality for displaying posts from database

--- app.py
+++ app.py
@@ -23,11 +23,18 @@ def get_db_connection():
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
-    
-    return "<h1>Welcome to My Blog</h1>"
+    # Get a connection to the database
+    conn = get_db_connection()
+
+    # Execute a query to fetch all posts from the "posts" table in the database
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+
+    # close the connection
+    conn.close()
+
+    # send the posts to the index.html template to be displayed
+    return render_template('index.html', posts=posts)
 
 
 # route to create a post
-
-
-app.run()
+app.run(port=5008)
