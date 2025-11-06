51f709e3328ba3cfb76fa245ecc4a8425535c43c
Pano Tsiftilis <panot@macbookpro.lan>
2025-11-06 15:29:12 -0600

    Added functionality for creating posts on the blog

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config["SECRET_KEY"] = 'your secret key'
 
 
 # Function to open a connection to the database.db file
@@ -37,4 +37,33 @@ def index():
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    # determine if the page is being requested with GET or POST
+    if request.method == 'POST':
+        # get the title and content from the submission
+        title = request.form['title']
+        content = request.form['content']
+
+        # display an error if title or content is missing
+        # else make a database connection and insert the new blog post
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            # insert data into database
+            conn = get_db_connection()
+
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+
+    return render_template('create.html')
+
+
+
+
 app.run(port=5008)
