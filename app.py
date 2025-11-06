a42e4a4dd618dc5f33bb01aac7b46558d1f59d03
Pano Tsiftilis <panot@macbookpro.lan>
2025-11-06 15:50:07 -0600

    Added functionality for editing posts

--- app.py
+++ app.py
@@ -19,6 +19,19 @@ def get_db_connection():
     #return the connection object
     return conn
 
+# Function to get post from database
+def get_post(post_id):
+    conn = get_db_connection()
+
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+
+    conn.close()
+
+    if post is None:
+        abort(404)
+
+    return post
+
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -63,6 +76,33 @@ def create():
 
     return render_template('create.html')
 
+# create a route to edit a post. load page with get or post method
+#pass the post id as url parameter
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    # get post from the database with a select query for post with that id
+    post = get_post(id)
+
+    # determine if the page is being requested with GET or POST
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        # If POST then process the form data. Get the data and validate it. Update post and redirect to homepage
+        
+        
+    # If GET then display it
+    return render_template('edit.html', post=post)
 
 
 
